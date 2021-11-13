76fbe2b4a3265863ffa6a3d77f46a51920fb8be3
Kirill <kirill-prohor@yandex.ru>
2021-11-13 15:30:13 +0300

    Fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,147 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const filename = (ext) => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
+
+process.on('warning', (warning) => {
+    console.log(warning.stack, ' DATA DEBUG');
+});
+  
+const plugins = () => {
+    const basePlugins = [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.resolve(__dirname, './src/index.html')
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'news.html',
+            template: path.resolve(__dirname, './src/news.html')
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'news_detailed.html',
+            template: path.resolve(__dirname, './src/news_detailed.html')
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'production.html',
+            template: path.resolve(__dirname, './src/production.html')
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'about.html',
+            template: path.resolve(__dirname, './src/about.html')
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'dummy.html',
+            template: path.resolve(__dirname, './src/dummy.html')
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'machinery.html',
+            template: path.resolve(__dirname, './src/machinery.html')
+        }),
+        new MiniCssExtractPlugin({
+            filename: `./css/${filename('css')}`
+        }),
+    ];
+  
+    if (isProd) {
+      basePlugins.push(
+        new ImageMinimizerPlugin({
+            minimizerOptions: {
+              // Lossless optimization with custom option
+              // Feel free to experiment with options for better result for you
+              plugins: [
+                ["gifsicle", { interlaced: true }],
+                ["jpegtran", { progressive: true }],
+                ["optipng", { optimizationLevel: 5 }]
+              ],
+            },
+        })
+      )
+    }
+  
+    return basePlugins;
+  };
 
 module.exports = {
-    entry: './src/index.js',
+    context: path.resolve(__dirname, 'src'),
+    entry: './index.js',
     output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'dist')
+        filename: `./js/${filename('js')}`,
+        path: path.resolve(__dirname, 'public'),
+        clean: true,
+        publicPath: '/'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        publicPath: '/',
+        open: true,
+        watchContentBase: true,
+        port: 8080,
+    },
+    target: 'web',
+    optimization: {
+        splitChunks: {
+        chunks: 'all'
+        },
+        minimize: false
     },
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader']
+                test: /\.(html)$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                {
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                    hmr: isDev
+                    },
+                },
+                    'css-loader'
+                ],
             }, 
             {
-                test: /\.scss$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+                test: /\.s[ac]ss$/,
+                use: [
+                  {
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                      publicPath: (resourcePath, context) => {
+                        return path.relative(path.dirname(resourcePath), context) + '/';
+                      },
+                    }
+                  },
+                  'css-loader',
+                  'sass-loader'
+                ],
             },
             {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
+                test: /\.(?:|gif|png|jpg|jpeg|svg)$/,
+                use: [{
+                  loader: 'file-loader',
+                  options: {
+                    name: `./img/${filename('[ext]')}`
+                  }
+                }],
             },
             {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
+                test: /\.(?:|eot|ttf|woff|woff2|otf)$/,
+                use: [{
+                  loader: 'file-loader',
+                  options: {
+                    name: `./fonts/${filename('[ext]')}`
+                  }
+                }],
             }
         ]
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: './src/index.html'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'news.html',
-            template: './src/news.html'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'news_detailed.html',
-            template: './src/news_detailed.html'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'production.html',
-            template: './src/production.html'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'about.html',
-            template: './src/about.html'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'dummy.html',
-            template: './src/dummy.html'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'machinery.html',
-            template: './src/machinery.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'index.css'
-        })
-    ]
+    plugins: plugins(),
+    devtool: isProd ? false : 'source-map'
 }
