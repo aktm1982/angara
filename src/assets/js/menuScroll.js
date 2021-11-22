24efea1df1b708446bd3abc4996cc8b4c35c6eea
Kirill <kirill-prohor@yandex.ru>
2021-11-22 11:55:11 +0300

    fix

--- src/assets/js/menuScroll.js
+++ src/assets/js/menuScroll.js
@@ -1,6 +1,5 @@
 
 window.addEventListener('scroll', () => {
-	console.log(window.pageYOffset);
 	if (window.pageYOffset > 0) {
 		document.querySelector('.header_f').classList.add('header--scroll')
 	}
